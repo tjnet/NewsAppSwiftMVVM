e15def93a71e506a184ef32d15fd15ce70d5a5cd
tjnet <rsa14631@nifty.com>
2016-11-09 23:41:24 +0900

    create BasePresenter

---
+++ NewsAppSwiftMVVM/NewsAppSwiftMVVM/BasePresenter.swift
@@ -0,0 +1,14 @@
+//
+//  BasePresenter.swift
+//  NewsAppSwiftMVVM
+//
+//  Created by TanakaJun on 2016/11/09.
+//  Copyright © 2016年 edu.self. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class BasePresenter {
+    let bag = DisposeBag()
+}
