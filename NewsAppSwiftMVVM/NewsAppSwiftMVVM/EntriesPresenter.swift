ef7c3ab69004c3486a617bd9bd023645db1dbb84
tjnet <rsa14631@nifty.com>
2016-11-09 23:59:33 +0900

    create UI class in CollectionView

---
+++ NewsAppSwiftMVVM/NewsAppSwiftMVVM/EntriesPresenter.swift
@@ -0,0 +1,17 @@
+//
+//  EntriesPresenter.swift
+//  NewsAppSwiftMVVM
+//
+//  Created by TanakaJun on 2016/11/09.
+//  Copyright © 2016年 edu.self. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class EntriesPresenter: BasePresenter {
+    
+    
+    
+    
+}
---
+++ NewsAppSwiftMVVM/NewsAppSwiftMVVM/ListEntriesUI.swift
@@ -0,0 +1,15 @@
+//
+//  ListEntriesUI.swift
+//  NewsAppSwiftMVVM
+//
+//  Created by TanakaJun on 2016/11/09.
+//  Copyright © 2016年 edu.self. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol ListEntriesUI: BaseUI {
+    func showEntries(entries: [Entry])
+    func appendEntries(entries: [Entry])
+}
