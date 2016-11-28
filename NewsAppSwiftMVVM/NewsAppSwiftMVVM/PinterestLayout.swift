4eed59bdc11b0dcc41580722dd52b2271f37b08f
tjnet <rsa14631@nifty.com>
2016-11-28 23:26:44 +0900

    create custom layout

---
+++ NewsAppSwiftMVVM/NewsAppSwiftMVVM/PinterestLayout.swift
@@ -0,0 +1,13 @@
+//
+//  PinterestLayout.swift
+//  NewsAppSwiftMVVM
+//
+//  Created by TanakaJun on 2016/11/28.
+//  Copyright © 2016年 edu.self. All rights reserved.
+//
+
+import UIKit
+
+class PinterestLayout: UICollectionViewLayout {
+
+}
