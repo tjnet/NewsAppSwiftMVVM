b40929c37474fd79ba2b6eba887c4b06110e8e60
tjnet <rsa14631@nifty.com>
2016-11-30 00:10:14 +0900

    create basis

---
+++ NewsAppSwiftMVVM/NewsAppSwiftMVVM/EntriesLayout.swift
@@ -0,0 +1,39 @@
+//
+//  PinterestLayout.swift
+//  NewsAppSwiftMVVM
+//
+//  Created by TanakaJun on 2016/11/28.
+//  Copyright © 2016年 edu.self. All rights reserved.
+//
+
+import UIKit
+
+protocol EntriesLayoutDelegate {
+
+    func collectionView(collectionView:UICollectionView, heightForPhotoAtIndexPath indexPath:NSIndexPath,
+                        withWidth:CGFloat) -> CGFloat
+
+    func collectionView(collectionView: UICollectionView,
+                        heightForAnnotationAtIndexPath indexPath: NSIndexPath, withWidth width: CGFloat) -> CGFloat
+}
+
+class EntriesLayout: UICollectionViewLayout {
+    
+    var delegate: EntriesLayoutDelegate!
+    
+    var numberOfColumns = 2
+    var cellPadding: CGFloat = 6.0
+    
+    private var cache = [UICollectionViewLayoutAttributes]()
+    
+    private var contentHeight: CGFloat = 0.0
+    
+    private var contentWidth: CGFloat {
+        let insets = collectionView!.contentInset
+        return CGRectGetWidth(collectionView!.bounds) - (insets.left + insets.right)
+    }
+    
+    
+    
+
+}
